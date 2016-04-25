aee2834c0b84d648abcf06618deb0370aaf977d2
Wiktor Wiejak <wiktor@tests-imac.local>
2016-04-25 20:03:34 +0100

    Initial Commit

---
+++ remote-car/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  remote-car
+//
+//  Created by Wiktor Wiejak on 25/04/2016.
+//  Copyright © 2016 Wiktor Wiejak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
